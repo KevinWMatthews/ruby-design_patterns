679e3eafd9faf6566b227559debda9674d10e085
Kevin Matthews <kmatthews@cyberdata.net>
2017-07-20 17:28:26 -0700

    Basic Employee

--- observer/src/observer.rb
+++ observer/src/observer.rb
@@ -1,1 +1,12 @@
 #!/usr/bin/env ruby
+
+class Employee
+  attr_reader :name, :title
+  attr_accessor :salary
+
+  def initialize(name, title, salary)
+    @name = name
+    @title = title
+    @salary = salary
+  end
+end
--- observer/test/test_observer.rb
+++ observer/test/test_observer.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env ruby
 
 require_relative '../src/observer.rb'
+
+fred = Employee.new("Fred", "AC Repairman", 30000.0)
+puts fred.salary
+# Give fred a raise
+fred.salary = 35000.0
+puts fred.salary
