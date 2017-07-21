1716f2bd4d5724df6ecf11ba1bbccce39cecbe5e
Kevin Matthews <kmatthews@cyberdata.net>
2017-07-20 18:00:33 -0700

    Add notes on push/pull variations of the Observer pattern

--- observer/test/test_observer_blocks.rb
+++ observer/test/test_observer_blocks.rb
@@ -6,6 +6,12 @@ fred = Employee.new("Fred", "AC Repairman", 30000.0)
 
 # These code blocks will be stored as Proc objects in the Subject class.
 fred.add_observer() do |changed_employee|
+  # This is an implementation of the 'pull' method;
+  # The Observer class pulls whatever data it needs from the Subject class.
+  # We could also implement the Observer pattering using the 'push' method.
+  # This would entail an update function such as:
+  # observer.update(self, :salary_changed, old_salary, new_salary)
+  # In this case, the Subject pushes all necessary data to the Subject.
   puts("Cut a new check for #{changed_employee.name}!")
   puts("His salary is now #{changed_employee.salary}!")
 end
